f35b5df8fe29e63d46aa44edee218145ab0ac467
edsonnaza <edsonnaza@gmail.com>
2023-01-02 18:41:56 -0300

    change project erased projects builded

--- src/data.js
+++ src/data.js
@@ -1,20 +1,6 @@
 export const projects = [
-  {
-    title: "Smart Reception",
-    subtitle: "PHP/Laravel/VUE/Linux LAMP.",
-    description:
-      "During the pandemic time, I developed a WebApp solution that allows customers to request service at the company's reception through the system. Whose main purpose was created to avoid direct contact with the secretary at the reception. With this solution, 90% of the proposed objective was achieved.",
-    image: "./smartreception2.gif",
-    link: "#",
-  },
-{
-    title: "Contacts",
-    subtitle: "PHP/Laravel/Linux LAMP.",
-    description:
-      "It is a simple but complete application to control customer contacts. Data that can be consumed by the sellers of the company, in addition to providing a process of orders indicating the status in three types, pending, accepted and rejected. The application was created in PHP, Laravel and Mysql. Linux LAMP server.",
-    image: "./Contacts.gif",
-    link: "#",
-  },
+  
+  
   {
     title: "React Tracks",
     subtitle: "React and Python",
