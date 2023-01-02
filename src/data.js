6c4c6ca9af2adf80aa61fb0c921728266f458a43
edsonnaza <edsonnaza@gmail.com>
2023-01-02 18:34:22 -0300

    link project defalt changed

--- src/data.js
+++ src/data.js
@@ -38,7 +38,7 @@ export const projects = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
     link: "https://reactbootcamp.com",
-  }
+  },
   
 ];
 
